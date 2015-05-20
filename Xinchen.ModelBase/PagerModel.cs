c6283b9648b3e7292842979e727c81a5d6d26f84
wzxinchen <1101385391@qq.com>
2015-05-20 20:16:30 +0800

    Signed-off-by: wzxinchen <1101385391@qq.com>

---
+++ Xinchen.ModelBase/PagerModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xinchen.ModelBase
+{
+    public class PagerModel
+    {
+        public int Page { get; set; }
+        public int Start { get; set; }
+        public int Limit { get; set; }
+    }
+}

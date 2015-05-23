c9fd1badbca5fe27b59a47a99c9e1d1b9bcf5dae
wzxinchen <1101385391@qq.com>
2015-05-23 22:35:05 +0800

    Signed-off-by: wzxinchen <1101385391@qq.com>

---
+++ ChinhDo.Transactions.FileManager/FolderMoveOperation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ChinhDo.Transactions
+{
+    public class FolderMoveOperation : IRollbackableOperation
+    {
+        private readonly string sourceFolderName;
+        private readonly string destFolderName;
+
+        /// <summary>
+        /// Instantiates the class.
+        /// </summary>
+        /// <param name="sourceFolderName">The name of the file to move.</param>
+        /// <param name="destFolderName">The new path for the file.</param>
+        public FolderMoveOperation(string sourceFolderName, string destFolderName)
+        {
+            this.sourceFolderName = sourceFolderName;
+            this.destFolderName = destFolderName;
+        }
+
+        public void Execute()
+        {
+            Directory.Move(sourceFolderName, destFolderName);
+        }
+
+        public void Rollback()
+        {
+            Directory.Move(destFolderName, sourceFolderName);
+        }
+    }
+}
---
+++ Xinchen.Utils/ServiceProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xinchen.Utils
+{
+    public class ServiceProvider
+    {
+        static Dictionary<Type, object> _serviceDict = new Dictionary<Type, object>();
+        public static T GetService<T>()
+        {
+            var type = typeof(T);
+            object service = null;
+            if (!_serviceDict.TryGetValue(type, out service))
+            {
+                service = Activator.CreateInstance(type, false);
+                _serviceDict.Add(type, service);
+            }
+            return (T)service;
+        }
+    }
+}
